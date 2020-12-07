8a17abf45c29757e6fe4c2d8ebf01a559b6b4453
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-12-07 11:56:14 -0800

    Creating drop down menu options to bind json data to.

--- static/js/app.js
+++ static/js/app.js
@@ -7,3 +7,24 @@
  * index 1 - metadata
  * index 2 - samples
  */
+
+//  Load data from json
+d3.json("../../samples.json").then(function(navelData) {
+    console.log(navelData);
+}).catch(function(error) {
+    console.log(error);
+  });
+
+var dropdownMenu = d3.select("#selDataset")
+
+dropdownMenu.selectAll()
+
+//  Drop down menu hanlder
+function handleDropDown() {
+    // Prevent page refresh
+    d3.event.preventDefault();
+
+    // Select input value from drop down menu
+    var dataset =  dropdownMenu.node().value;
+    console.log(dataset);
+}
