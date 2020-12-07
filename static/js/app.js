371a9af2145bbcd457ee991200bbbaf6351fe270
rarconaut <68246130+rarconaut@users.noreply.github.com>
2020-12-07 10:46:57 -0800

    Added helper function to select json data.

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Helper function to select navel data
+ * Returns an array of values
+ * @param {array} rows
+ * @param {integer} index
+ * index 0 - names
+ * index 1 - metadata
+ * index 2 - samples
+ */
